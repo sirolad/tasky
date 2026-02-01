5599c1021ee6ef8a8a73a17caa58a088935b7152
Surajudeen Akande <sirolad@gmail.com>
2026-02-01 23:04:08 +0100

    restructure folders

--- prisma.config.ts
+++ prisma.config.ts
@@ -1,10 +1,11 @@
-import { defineConfig } from "prisma/config";
+import 'dotenv/config';
+import { defineConfig } from 'prisma/config';
 
 export default defineConfig({
-  schema: "prisma/schema.prisma",
+  schema: 'prisma/schema.prisma',
   migrations: {
-    path: "prisma/migrations",
-    seed: "prisma/seed.ts",
+    path: 'prisma/migrations',
+    seed: 'prisma/seed.ts',
   },
   datasource: {
     url: process.env.DATABASE_URL,
