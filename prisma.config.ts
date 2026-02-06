96f0df248e5e9c55f4f1e77cf0135c8d16fbfa7d
Surajudeen Akande <sirolad@gmail.com>
2026-02-06 12:39:51 +0100

    let docker use seeder

--- prisma.config.ts
+++ prisma.config.ts
@@ -5,7 +5,7 @@ export default defineConfig({
   schema: 'prisma/schema.prisma',
   migrations: {
     path: 'prisma/migrations',
-    seed: 'prisma/seed.ts',
+    seed: 'ts-node --transpile-only prisma/seed.ts',
   },
   datasource: {
     url: process.env.DATABASE_URL,
